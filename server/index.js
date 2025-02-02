1222c5aa20b47e2ed6489314ad138ad2c6ea0e3a
brandon <brandonawilliams1@gmail.com>
2025-02-01 20:38:52 -0600

    mount route in index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const mongoose = require('mongoose');
+//mount the route
+const exampleRoutes = require('./routes/exampleRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+app.use('/api', exampleRoutes);//mount the route in middleware
 
 //test route
 app.get('/', (req, res) => {
