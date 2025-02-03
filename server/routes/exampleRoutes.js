39a51aac9d5292eb6e13abed9e43e77be06dc212
brandon <brandonawilliams1@gmail.com>
2025-02-03 01:33:27 -0600

    post route config and successful get/post req

--- server/routes/exampleRoutes.js
+++ server/routes/exampleRoutes.js
@@ -2,12 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Example = require("../models/Example");
 
+// GET all examples
 router.get('/examples', async (req, res) => {
     try {
         const examples = await Example.find();
         res.json(examples);
     } catch (err) {
         res.status(500).json({ message: err.message });
+    }
+});
+
+//POST ROUTE
+router.post('/examples', async (req, res) => {
+    try {
+        const newExample = new Example({
+            address: req.body.address,
+            isHomerun: req.body.isHomerun
+
+        });
+
+        const savedExample = await newExample.save();
+        res.status(201).json(savedExample);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
     }
 });
 
